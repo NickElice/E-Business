db69e4e2558304a6df06950c28f94e058015ef1b
Nick Elice <nelice@stud.hs-offenburg.de>
2022-05-10 15:34:55 +0200

    hallo

--- php/login.php
+++ php/login.php
@@ -1,13 +1,12 @@
 
 <?php
 
-use LDAP\Result;
-if(isset($_GET["benutzer"]) && isset($_GET["passwort"]) ){
+
 $mysqli = new mysqli("localhost", "root", "", "mydb");
 if ($mysqli->connect_errno) {
 	die("Verbindung fehlgeschlagen: " . $mysqli->connect_error);
 }
-$sql = "SELECT * FROM mydb.user where user.email = ? and password = ?";
+$sql = "SELECT * FROM mydb.produkt";
 $statement = $mysqli->prepare($sql);
 $statement->execute();
 
@@ -17,5 +16,5 @@ while ($row = $result->fetch_assoc()) {
 	print $row["produktName"];
 	print "<br>";
 }
-}
+
 ?>
