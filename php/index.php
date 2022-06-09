3346d8751f436e2ab1240b61faa67c250ad13701
= <=>
2022-06-09 15:30:15 +0200

    Dynamische Produktanzeige 0.7

--- php/index.php
+++ php/index.php
@@ -2,8 +2,8 @@
 
 use LDAP\Result;
 
-//if (isset($_GET["benutzer"]) && isset($_GET["passwort"]) ) {
-	/*
+//if (isset($_GET['benutzer']) && isset($_GET['passwort']) ) {
+	print("
 	<!DOCTYPE html>
 	<html>
 	
@@ -14,47 +14,62 @@ use LDAP\Result;
 	  <meta name='viewport' content='width=device-width, initial-scale=1'>
 	  <link rel='stylesheet' href='../css/index.css'>
 	  <!-- CSS only -->
-	  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-		integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	  <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet'
+		integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>
 	
-	  <script src="https://kit.fontawesome.com/b05822756c.js" crossorigin="anonymous"></script>
+	  <script src='https://kit.fontawesome.com/b05822756c.js' crossorigin='anonymous'></script>
 	</head>
-	*/
-$mysqli = new mysqli("localhost", "root", "", "ebusiness");
+	");
+print("<body>");
+$mysqli = new mysqli('localhost', 'root', '', 'ebusiness');
 if ($mysqli->connect_errno) {
-	die("Verbindung fehlgeschlagen: " . $mysqli->connect_error);
+	die('Verbindung fehlgeschlagen: ' . $mysqli->connect_error);
 }
-$sql = "SELECT * FROM ebusiness.benutzer ";
+$sql = 'SELECT * FROM ebusiness.benutzer ';
 $statement = $mysqli->prepare($sql);
 $statement->execute();
 
 $result = $statement->get_result();
-
+print("<section class='py-5'>
+<div class='container  mt-5 px-lg-5'>
+<div class='row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center'>");
 while ($row = $result->fetch_assoc()) {
-	print('<div class="col mb-5">
-          <div class="card h-120"><img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
-              alt="Bild von Produkt">
-            <div class="card-body p-4">
-              <div class="text-center">
-                <h5 class="fw-bolder">Item 1</h5>7€
-              </div>
-            </div>
-            <div class="card-footer p-4 border-top-0 pt-0 bg-transparent">
-              <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Zu Warenkorb hinzufügen</a>
-              </div>
-            </div>
-          </div>
-        </div>
-	');
+	print("<div class='col mb-5'>
+	<div class='card h-120'><img class='card-img-top' src='https://dummyimage.com/450x300/dee2e6/6c757d.jpg'
+		alt='Bild von Produkt'>
+	  <div class='card-body p-4'>
+		<div class='text-center'>
+		  <h5 class='fw-bolder'>Item 1</h5>7€
+		</div>
+	  </div>
+	  <div class='card-footer p-4 border-top-0 pt-0 bg-transparent'>
+		<div class='text-center'><a class='btn btn-outline-dark mt-auto' href='#'>Zu Warenkorb hinzufügen</a>
+		</div>
+	  </div>
+	</div>
+  </div>
+
+
+
+
+
+
+	");
 }
-	
-//	print("</html>");
+print("
+			</div>
+			</div>
+			</section>
+");
+print("</Body>");
+		print("</html>");
 //	if ($count == 1) {
 //		session_start();
 //		$_SESSION['username']=$user;
-//		header("location: home.php");
+//		header('location: home.php');
 //	} else {
-//		header("location: login.html");
+//		header('location: login.html');
 //	}
 
 //}
+?>
